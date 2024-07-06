22322f8a240d250d249fc1b154c4df71dedc1495
Brandi Burnell <brandiburnell6@gmail.com>
2024-07-06 13:25:06 -0600

    add api endpoints

--- server/app.py
+++ server/app.py
@@ -20,10 +20,85 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+api = Api(app)
 
 @app.route('/')
 def home():
     return ''
 
+class Campers(Resource):
+    def get(self):
+        try:
+            campers = [camper.to_dict(only=("id", "name", "age")) for camper in Camper.query.all()]
+
+            return make_response(campers, 200)
+        except:
+            raise make_response('bad request', 400)
+        
+    def post(self):
+        try:
+            new_camper = {
+                "name" : request.form['name'],
+                "age" : request.form['age']
+            }
+
+            db.session.add(new_camper)
+            db.session.commit()
+
+            return make_response(new_camper.to_dict(only=("id", "name", "age")), 201)
+        except:
+            return { "error": "400: Validation error"}, 400
+    
+api.add_resource(Campers, '/campers')
+
+class CamperByID(Resource):
+    def get(self, id):
+        try:
+            camper = Camper.query.filter_by(id=id).first()
+            
+            return make_response(camper.to_dict(), 200)
+        except:
+            return make_response({"error": "Camper not found"}, 400)
+        
+    def patch(self, id):
+        try:
+            camper = Camper.query.filter_by(id=id).first()
+
+            print(request.form)
+            for attr in request.form:
+                print(attr)
+                setattr(camper, attr, request.form[attr])
+
+            db.session.add(camper)
+            db.session.commit()
+
+            return make_response(camper.to_dict(only=("id", "name", "age")), 200)
+        except:
+            return make_response({"error": "Camper not found"}, 400)
+            
+class Activities(Resource):
+    def get(self):
+        activities = [activity.to_dict(only=("id", "name", "difficulty")) for activity in Activity.query.all()]
+
+        return make_response(activities, 200)
+    
+api.add_resource(Activities, '/activities')
+
+class ActivityByID(Resource):
+    def delete(self, id):
+        try:
+            activity = Activity.query.filter_by(id=id).first()
+
+            db.session.delete(activity)
+            db.session.commit()
+            
+            return make_response("", 202)
+        except:
+            return make_response({"error": "Activity not found"}, 400)
+
+api.add_resource(ActivityByID, '/activities/<int:id>')
+
+
+api.add_resource(CamperByID, '/campers/<int:id>')
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
