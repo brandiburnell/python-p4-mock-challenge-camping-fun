1ba09ad2359197c9e789823f3d934212950d56b3
Brandi Burnell <brandiburnell6@gmail.com>
2024-07-06 12:12:58 -0600

    add validations

--- server/models.py
+++ server/models.py
@@ -23,7 +23,10 @@ class Activity(db.Model, SerializerMixin):
     name = db.Column(db.String)
     difficulty = db.Column(db.Integer)
 
+    # Add relationship
     signups = db.relationship("Signup", cascade="all,delete", backref="activity")
+    
+    # Add serialization rules
     serialize_rules = ("-signups.activity",)
 
 class Camper(db.Model, SerializerMixin):
@@ -33,10 +36,24 @@ class Camper(db.Model, SerializerMixin):
     name = db.Column(db.String, nullable=False)
     age = db.Column(db.Integer)
 
+    # Add relationship
     signups = db.relationship("Signup", backref="camper")
 
+    # Add serialization rules
     serialize_rules = ("-signups.camper",)
 
+    # Add validation
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError("Camper must have a name")
+        return name
+    
+    @validates('age')
+    def validate_age(self, key, age):
+        if age < 8 or age > 18:
+            raise ValueError("Age must be between 8 and 18")
+        return age
 
 class Signup(db.Model, SerializerMixin):
     __tablename__ = 'signups'
@@ -46,6 +63,17 @@ class Signup(db.Model, SerializerMixin):
     camper_id = db.Column(db.Integer, db.ForeignKey("campers.id"))
     activity_id = db.Column(db.Integer, db.ForeignKey("activities.id"))
 
+    # Add relationships
+
+    # Add serialization rules
     serialize_rules = ("-camper.signups", "-activity.signups")
+
+    # Add validation
+    @validates('time')
+    def validates_time(self, key, time):
+        if time < 0 or time > 23:
+            raise ValueError('time must be between 0 and 23')
+        return time
+
 
 # add any models you may need.
